7b0162a5d56e884fb29a4950e7bbcf9974e8a4a7
D A Ritesh <92038288+RITESH1218@users.noreply.github.com>
2023-05-14 19:56:00 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -3,69 +3,53 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
-{   EditText t1,t2,res;
-    Button addbtn,subbtn,mulbtn,divbtn;
-
+{   EditText t1,res;
+    RadioButton r1,r2,r3,r4;
+    Button btn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) 
+    protected void onCreate(Bundle savedInstanceState)
     {   super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        t1 = (EditText) findViewById(R.id.t1);
-        t2 = (EditText) findViewById(R.id.t2);
-        res = (EditText) findViewById(R.id.res);
-        addbtn = (Button) findViewById(R.id.addbtn);
-        subbtn = (Button) findViewById(R.id.subbtn);
-        mulbtn = (Button) findViewById(R.id.mulbtn);
-        divbtn = (Button) findViewById(R.id.divbtn);
+        t1=(EditText)findViewById(R.id.t1);
+        res=(EditText)findViewById(R.id.res);
+        r1=(RadioButton)findViewById(R.id.r1);
+        r2=(RadioButton)findViewById(R.id.r2);
+        r3=(RadioButton)findViewById(R.id.r3);
+        r4=(RadioButton)findViewById(R.id.r4);
+        btn=(Button)findViewById(R.id.btn);
 
-
-        addbtn.setOnClickListener(new View.OnClickListener() 
+        btn.setOnClickListener(new View.OnClickListener()
         {   @Override
-            public void onClick(View v) {
-                String v1 = t1.getText().toString();
-                String v2 = t2.getText().toString();
-                int a = Integer.parseInt(v1);
-                int b = Integer.parseInt(v2);
-                int c = a + b;
-                res.setText("Result =" + c); }
-        });
-
-        subbtn.setOnClickListener(new View.OnClickListener()
-        {   @Override
-            public void onClick(View v) {
-                String v1 = t1.getText().toString();
-                String v2 = t2.getText().toString();
-                int a = Integer.parseInt(v1);
-                int b = Integer.parseInt(v2);
-                int c = a - b;
-                res.setText("Result =" + c); }
-        });
-
-        mulbtn.setOnClickListener(new View.OnClickListener()
-        {   @Override
-            public void onClick(View v) {
-                String v1 = t1.getText().toString();
-                String v2 = t2.getText().toString();
-                int a = Integer.parseInt(v1);
-                int b = Integer.parseInt(v2);
-                int c = a * b;
-                res.setText("Result =" + c); }
-        });
-
-
-        divbtn.setOnClickListener(new View.OnClickListener() 
-        {   @Override
-            public void onClick(View v) {
-                String v1 = t1.getText().toString();
-                String v2 = t2.getText().toString();
-                int a = Integer.parseInt(v1);
-                int b = Integer.parseInt(v2);
-                int c = a / b;
-                res.setText("Result =" + c); }
+            public void onClick(View v)
+            {   String a=t1.getText().toString();
+                float aa=Float.parseFloat(a);
+                float c;
+                if(r1.isChecked())
+                {   c=aa/100;
+                    res.setText("Result ="+c);
+                }
+                else if(r2.isChecked())
+                {   c=aa*100;
+                    res.setText("Result ="+c);
+                }
+                else if(r3.isChecked())
+                {   c=aa*12;
+                    res.setText("Result ="+c);
+                }
+                else if(r4.isChecked())
+                {   c=aa/12;
+                    res.setText("Result ="+c);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Choose any one radio button", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
